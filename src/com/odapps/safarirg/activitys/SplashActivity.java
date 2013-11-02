a91e705b9a1e10c40a4262327973917d4b80cb5e
DaNiStO <danielstolero@gmail.com>
2013-11-02 12:24:03 +0200

    שינוי ראשוני

--- src/com/odapps/safarirg/activitys/SplashActivity.java
+++ src/com/odapps/safarirg/activitys/SplashActivity.java
@@ -14,6 +14,8 @@ public class SplashActivity extends Activity {
 		setContentView(R.layout.activity_splash);
 	}
 
+	// בדיקת שינוי
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
